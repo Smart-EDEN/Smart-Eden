7370a7c1033dfc4eb82cbb8eb95de2f3d5917cf6
fatjoe3012 <lenk9797@gmail.com>
2023-03-24 17:20:32 +0100

    test action approval

--- app/src/main/java/com/example/smarteden/FirstFragment.kt
+++ app/src/main/java/com/example/smarteden/FirstFragment.kt
@@ -14,11 +14,9 @@ import com.example.smarteden.databinding.FragmentFirstBinding
 class FirstFragment : Fragment() {
 
     private var _binding: FragmentFirstBinding? = null
-
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
