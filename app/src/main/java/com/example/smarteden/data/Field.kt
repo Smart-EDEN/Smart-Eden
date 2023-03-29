425fc9066201488b975d356e709c3c6539e3094c
ManuelKiendl <80915934+ManuelKiendl@users.noreply.github.com>
2023-03-29 22:56:46 +0200

    new recyclerview for the fields

---
+++ app/src/main/java/com/example/smarteden/data/Field.kt
@@ -0,0 +1,5 @@
+package com.example.smarteden.data
+
+class Field {
+
+}
---
+++ app/src/main/java/com/example/smarteden/greenhousefields/RecyclerViewAdapter.kt
@@ -0,0 +1,40 @@
+package com.example.smarteden.greenhousefields
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.smarteden.R
+class CardViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    val titleTextView= itemView.findViewById<TextView>(R.id.textview_title)
+    val subtitleTextView = itemView.findViewById<TextView>(R.id.textview_subtitle)
+    val descriptionTextView = itemView.findViewById<TextView>(R.id.textview_description)
+
+}
+class RecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            val itemView = LayoutInflater.from(parent.context).inflate(R.layout.cardviewfield, parent, false)
+            val layoutParams = itemView.layoutParams as RecyclerView.LayoutParams
+            layoutParams.width = (parent.width * 0.5).toInt()
+            itemView.layoutParams = layoutParams
+            return CardViewHolder(itemView)
+        }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            val cardViewHolder = holder as CardViewHolder
+            val title = "Card ${position+1} Title"
+            val subtitle = "Card ${position+1} Subtitle"
+            val description = "Card ${position+1} Description"
+            cardViewHolder.titleTextView.text = title
+            cardViewHolder.subtitleTextView.text = subtitle
+            cardViewHolder.descriptionTextView.text = description
+        }
+
+        override fun getItemCount(): Int {
+            return 10
+        }
+
+    }
