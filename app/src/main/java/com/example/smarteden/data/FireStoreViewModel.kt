ef1529dbc8ae728cf1f186d22659342daf8875b3
fatjoe3012 <lenk9797@gmail.com>
2023-03-31 12:52:25 +0200

    Add recyclerView to home fragment * get live updates of greenhouse changes * get only connected greenhouses

--- app/src/main/java/com/example/smarteden/data/FireStoreViewModel.kt
+++ app/src/main/java/com/example/smarteden/data/FireStoreViewModel.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
@@ -14,19 +15,22 @@ class FireStoreViewModel : ViewModel() {
     val userInformation: LiveData<User>
         get() = _userInformation
 
-    private var _greenHouses = MutableLiveData<ArrayList<Greenhouse>>()
+    private var _greenhouses = MutableLiveData<ArrayList<Greenhouse>>()
+        //getLiveGreenhouses() as MutableLiveData<ArrayList<Greenhouse>>
     val greenhouses: LiveData<ArrayList<Greenhouse>>
-        get() = _greenHouses
+        get() = _greenhouses
 
     private val db = Firebase.firestore
 
     fun setUser(newUserId: String) {
         currentUserId = newUserId
         getUserInformation()
-        getGreenhouses()
+        getLiveGreenhouses()
+        Log.d("BeneView", "Greenhouse war called")
     }
 
     fun addUser(userId: String, email: String) {
+        currentUserId = userId
         db.collection(USER_COLLECTION).document(userId).set(
             hashMapOf(
                 "name" to "",
@@ -36,34 +40,108 @@ class FireStoreViewModel : ViewModel() {
         )
     }
 
+    fun connectGreenhouseWithUser(serialNumber: String, password: String) {
+        db.collection(GREENHOUSE_COLLECTION).document(serialNumber).get()
+            .addOnSuccessListener { document ->
+                if (document != null) {
+                    if (document["password"].toString() == password) {
+                        //Password and serial number correct
+                        checkSerialNumbersOfUser(serialNumber)
+                    } else {
+                        //Password false
+                    }
+                } else {
+                    Log.d(TAG, "No such document")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.d(TAG, "get failed with ", exception)
+            }
+    }
+
+    private fun checkSerialNumbersOfUser(serialNumber: String) {
+        val user = userInformation.value!!
+        var serialNumberAlreadyExists = false
+        user.greenhouseIds.forEach { id ->
+            if (id == serialNumber)
+                serialNumberAlreadyExists = true
+        }
+        Log.d("CheckSerialNumber", serialNumberAlreadyExists.toString())
+        if (!serialNumberAlreadyExists)
+            addSerialNumber(serialNumber)
+
+        /*db.collection(USER_COLLECTION).document(currentUserId).get()
+            .addOnSuccessListener { document ->
+                if (document != null) {
+                    var idAlreadyExists = false
+                    val serialNumbersList = document["greenhouseIds"] as ArrayList<String>
+                    serialNumbersList.forEach { id ->
+                        if(id == serialNumber)
+                            idAlreadyExists = true
+                    }
+                    if(!idAlreadyExists) {
+                        serialNumbersList.add(serialNumber)
+                        addSerialNumber(serialNumber, serialNumbersList)
+                    }
+                } else {
+                    Log.d(TAG, "No such document")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.d(TAG, "get failed with ", exception)
+            }*/
+    }
+
+    private fun addSerialNumber(serialNumber: String) {
+        Log.d("Bene", "addSerialNumber")
+        val updates = hashMapOf<String, Any>(
+            "greenhouseIds" to FieldValue.arrayUnion(serialNumber)
+        )
+        db.collection(USER_COLLECTION).document(currentUserId).update(
+            updates
+
+        )
+    }
+
     fun getUserInformation() {
         db.collection(USER_COLLECTION).document(currentUserId).addSnapshotListener { doc, error ->
             if (error != null) {
                 Log.w(TAG, "Listen failed.", error)
                 return@addSnapshotListener
             }
+            val greenhouseIds = doc!!.data?.get("greenhouseIds") as ArrayList<*>
             val user = User(
-                doc!!.id,
+                doc.id,
                 doc.data!!["name"].toString(),
                 doc.data!!["email"].toString(),
-                ArrayList()
+                greenhouseIds as ArrayList<String>
             )
             _userInformation.value = user
         }
     }
 
-    fun getGreenhouses() {
+
+    fun getLiveGreenhouses(): LiveData<ArrayList<Greenhouse>> {
+        val liveGreenhouses = MutableLiveData<ArrayList<Greenhouse>>()
+        val listGreenhouses = ArrayList<Greenhouse>()
         db.collection(GREENHOUSE_COLLECTION).addSnapshotListener { value, error ->
+            listGreenhouses.clear()
             if (error != null) {
                 Log.w(TAG, "Listen failed.", error)
                 return@addSnapshotListener
             }
 
             for (doc in value!!) {
-                val greenhouse = Greenhouse(doc.id)
-                _greenHouses.value?.add(greenhouse)
+                val greenhouse = Greenhouse(doc.id, doc.data["name"].toString())
+                userInformation.value?.greenhouseIds?.forEach { greenhouseId ->
+                    if(greenhouse.id == greenhouseId)
+                        listGreenhouses.add(greenhouse)
+                }
             }
+            liveGreenhouses.value = listGreenhouses
+            _greenhouses = liveGreenhouses
         }
+        return liveGreenhouses
     }
 
     companion object {
