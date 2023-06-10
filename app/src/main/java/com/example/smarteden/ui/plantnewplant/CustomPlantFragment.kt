4ae8414c262638d31267a0e3c7eb917bc00d86fe
fatjoe3012 <lenk9797@gmail.com>
2023-06-10 13:19:43 +0200

    Custom plant * edit custom plant to plant custom plant * navigate back when planted

--- app/src/main/java/com/example/smarteden/ui/plantnewplant/CustomPlantFragment.kt
+++ app/src/main/java/com/example/smarteden/ui/plantnewplant/CustomPlantFragment.kt
@@ -1,19 +1,31 @@
 package com.example.smarteden.ui.plantnewplant
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import com.example.smarteden.R
+import com.example.smarteden.data.FieldViewModel
+import com.example.smarteden.data.Plant
+import com.example.smarteden.data.PlantViewModel
 import com.example.smarteden.databinding.FragmentCustomPlantBinding
+import com.google.android.material.datepicker.MaterialDatePicker
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class CustomPlantFragment : Fragment() {
 
     //private val firestoreViewModel: FireStoreViewModel by activityViewModels()
-    //private val fieldViewModel: FieldViewModel by activityViewModels()
+    private val fieldViewModel: FieldViewModel by activityViewModels()
+    private val plantViewModel: PlantViewModel by activityViewModels()
 
     private var _binding: FragmentCustomPlantBinding? = null
+    private var dateLong = 0L
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -31,10 +43,111 @@ class CustomPlantFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val datePicker =
+            MaterialDatePicker.Builder.datePicker()
+                .setTitleText("Select date")
+                .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
+                .build()
+        binding.btnPickDate.setOnClickListener {
+            datePicker.show(parentFragmentManager, "Bier")
+        }
+
+        datePicker.addOnPositiveButtonClickListener {newTime ->
+            Log.d("Bier", newTime.toString())
+            dateLong = newTime
+            val date = convertLongToTime(newTime)
+            binding.customPlantDate.text = "Ausgewähltes Datum: $date"
+            // Respond to positive button click.
+        }
+        datePicker.addOnNegativeButtonClickListener {
+            // Respond to negative button click.
+        }
+        datePicker.addOnCancelListener {
+            // Respond to cancel button click.
+        }
+        datePicker.addOnDismissListener {
+            // Respond to dismiss events.
+        }
+
+        binding.btnSend.setOnClickListener {
+
+            val plant = getPlantData()
+            fieldViewModel.plantNewPlantInField(plant, dateLong)
+            plantViewModel.saveNewPlant(plant)
+            findNavController().navigate(R.id.action_customPlantFragment_to_fieldoverviewFragment)
+        }
+    }
+
+    private fun getPlantData(): Plant {
+        val name = binding.customName.editText?.text.toString()
+        val waterAmountId = binding.amountWatering.checkedRadioButtonId
+        var waterAmount = MUCH
+        when (waterAmountId) {
+            binding.radioButton1.id -> {
+                waterAmount = MUCH
+            }
+            binding.radioButton2.id -> {
+                waterAmount = ZW_MUCH_MIDDLE
+            }
+            binding.radioButton3.id -> {
+                waterAmount = MIDDLE
+            }
+            binding.radioButton4.id -> {
+                waterAmount = ZW_MIDDLE_LESS
+            }
+            binding.radioButton5.id -> {
+                waterAmount = LESS
+            }
+        }
+        val wateringFrequencyId = binding.frequencyWatering.checkedRadioButtonId
+        var wateringFrequency = OFTEN
+        when (wateringFrequencyId) {
+            binding.radioButtonOften.id -> {
+                wateringFrequency = OFTEN
+            }
+            binding.radioButtonMiddle.id -> {
+                wateringFrequency = MIDDLE
+            }
+            binding.radioButtonRarely.id -> {
+                wateringFrequency = RARELY
+            }
+        }
+        val ripeDuration = binding.customDuration.editText?.text.toString().toInt()
+        val plantMonths = binding.customPlantMonths.editText?.text.toString()
+        val harvestMonths = binding.customHarvestMonths.editText?.text.toString()
+        val generalInfo = binding.customGeneralInfo.editText?.text.toString()
+
+        return Plant(
+            id = "1",
+            name,
+            ripeDuration,
+            plantMonths,
+            harvestMonths,
+            generalInfo,
+            waterAmount,
+            wateringFrequency
+        )
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun convertLongToTime(time: Long): String {
+        val date = Date(time)
+        val format = SimpleDateFormat("dd.MM.yyyy")
+        return format.format(date)
+    }
+
+    companion object{
+        private const val MUCH = 5
+        private const val ZW_MUCH_MIDDLE = 4
+        private const val MIDDLE = 3
+        private const val ZW_MIDDLE_LESS = 2
+        private const val LESS = 1
+
+        private const val OFTEN = 5
+        private const val RARELY = 1
     }
 }
--- app/src/main/java/com/example/smarteden/ui/plantnewplant/PlantNewPlantFragment.kt
+++ app/src/main/java/com/example/smarteden/ui/plantnewplant/PlantNewPlantFragment.kt
@@ -11,18 +11,27 @@ import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.smarteden.R
+import com.example.smarteden.data.FieldViewModel
 import com.example.smarteden.data.Plant
 import com.example.smarteden.data.PlantViewModel
 import com.example.smarteden.databinding.FragmentPlantNewPlantBinding
+import com.google.android.material.datepicker.MaterialDatePicker
 
 class PlantNewPlantFragment : Fragment() {
 
     //private val firestoreViewModel: FireStoreViewModel by activityViewModels()
-    //private val fieldViewModel: FieldViewModel by activityViewModels()
+    private val fieldViewModel: FieldViewModel by activityViewModels()
     private val plantViewModel: PlantViewModel by activityViewModels()
 
     private var _binding: FragmentPlantNewPlantBinding? = null
 
+    private lateinit var plant: Plant
+
+    val datePicker =
+        MaterialDatePicker.Builder.datePicker()
+            .setTitleText("Select date")
+            .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
+            .build()
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
@@ -54,6 +63,20 @@ class PlantNewPlantFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        datePicker.addOnPositiveButtonClickListener {newTime ->
+            fieldViewModel.plantNewPlantInField(plant, newTime)
+            findNavController().navigate(R.id.action_plantNewPlantFragment_to_fieldoverviewFragment)
+            // Respond to positive button click.
+        }
+        datePicker.addOnNegativeButtonClickListener {
+            // Respond to negative button click.
+        }
+        datePicker.addOnCancelListener {
+            // Respond to cancel button click.
+        }
+        datePicker.addOnDismissListener {
+            // Respond to dismiss events.
+        }
 
 
         binding.btnCustom.setOnClickListener {
@@ -66,7 +89,9 @@ class PlantNewPlantFragment : Fragment() {
         _binding = null
     }
 
-    fun clickPlant(plant: Plant) {
+    fun clickPlant(newPlant: Plant) {
+        datePicker.show(parentFragmentManager, "PlantNewPlant")
+        plant = newPlant
         Log.d("TODO", "database delete field infos $plant")
     }
 }
